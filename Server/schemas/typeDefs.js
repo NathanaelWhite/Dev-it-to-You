caa33f56705b6db6ead00d7da83a8530518d179d
Nathanael White <narhanaelw99@gmail.com>
2021-08-18 20:28:18 -0500

    created typeDefs for user, tag, auth, queries, and mutations

---
+++ Server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    connections: [User]
+    description: String
+    tags: [Tag]
+  }
+  type Tag {
+    _id: ID
+    name: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+      description: String
+      tags: [Tag]
+    ): Auth
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+      description: String
+      tags: [Tag]
+    ): User
+  }
+`;
+
+module.exports = typeDefs;
