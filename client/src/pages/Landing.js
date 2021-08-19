d1754d411813b72edf9b42175f2fcc7da4b1e145
HP-Adam <adamhpeterman@gmail.com>
2021-08-18 21:08:32 -0500

    work in progress

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Landing = () => {
+  const classes = useStyles();
+
   return (
     <main>
-      <h1>Dev Me Up</h1>
-      <p>Let'd find some developers and make a project out of this world!</p>
-      <button>Login</button>
-      <button>Sign Up</button>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <h1>Dev Me Up</h1>
+            </Paper>
+          </Grid>
+          <p>
+            Let'd find some developers and make a project out of this world!
+          </p>
+          <button>Login</button>
+          <button>Sign Up</button>
+        </Grid>
+      </div>
     </main>
   );
 };
