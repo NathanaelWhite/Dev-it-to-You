60f1f41b563c14034f6f0d70af81d1bebd0618d6
HP-Adam <adamhpeterman@gmail.com>
2021-08-18 20:34:45 -0500

    landing, header, footer skeleton - no styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Landing from "./pages/Landing";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <div>
+        {window.location.pathname !== "/" && <Header />}
+        <div>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <main>
+      <h1>Dev Me Up</h1>
+      <p>Let'd find some developers and make a project out of this world!</p>
+      <button>Login</button>
+      <button>Sign Up</button>
+    </main>
+  );
+};
+export default Landing;
