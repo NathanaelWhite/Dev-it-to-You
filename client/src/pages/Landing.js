c13f3c25f39c17dbbf4a2545d05b51594043daff
HP-Adam <adamhpeterman@gmail.com>
2021-08-19 23:24:39 -0500

    styling on landing, not finished

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    textAlign: "center",
+  },
+}));
 
 const Landing = () => {
+  const classes = useStyles();
+
   return (
-    <main>
-      <div>
-        <h1>Dev Me Up</h1>
-        <p>Let'd find some developers and make a project out of this world!</p>
-        <button>Login</button>
-        <button>Sign Up</button>
-      </div>
+    <main className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        justifyContent="center"
+        alignContent="center"
+      >
+        <Typography variant="h1">Dev Me Up</Typography>
+        <Typography variant="body1">
+          Let'd find some developers and make a project out of this world!
+        </Typography>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+        <Button variant="contained" color="primary">
+          Sign Up
+        </Button>
+      </Grid>
     </main>
   );
 };
