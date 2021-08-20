4bdab77bf5004a5f1bcc7d75b9cea464d4aeb405
Nathanael White <narhanaelw99@gmail.com>
2021-08-20 18:12:54 -0500

    added apollo provider to hook in the server to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,6 +15,25 @@ import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import SignUp from "./pages/signup-form";
 import Login from "./pages/login";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 // const theme = createTheme({
 //   palette: {
@@ -18,17 +44,19 @@ import Login from "./pages/login";
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-        <Footer />
-      </Router>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
