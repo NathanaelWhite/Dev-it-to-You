8f6185849fb0bad2d446f6d0dc4aa04e33c00587
Joshua D Wood <joshuawd190@gmail.com>
2021-08-22 01:45:41 -0500

    Feature(Faker Tags):
    Update seed file to include a function that generates
    tags for the faked user information

--- Server/seeders/seeds.js
+++ Server/seeders/seeds.js
@@ -1,9 +1,40 @@
-const faker = require("faker");
+const faker = require('faker');
 
-const db = require("../config/connection");
-const { User } = require("../models");
+const db = require('../config/connection');
+const { User } = require('../models');
 
-db.once("open", async () => {
+const tags = [
+  'Front End',
+  'Back End',
+  'HTML',
+  'CSS',
+  'Java',
+  'JavaScript',
+  'Python',
+  'SQL',
+  'PHP',
+  '.NET',
+  'Angular',
+  'React',
+  'SQL',
+  'NoSQL',
+  'Sequelize',
+  'C++',
+  'ExpressJs',
+  'NodeJs',
+  'GraphQL',
+];
+
+const generateTagString = () => {
+  const tagString = [];
+  tags.map((tag) => {
+    if (Math.floor(Math.random() * 10 + 1) > 5) {
+      tagString.push(tag);
+    }
+  });
+  return tagString.join(', ');
+};
+db.once('open', async () => {
   await User.deleteMany({});
 
   // create user data
@@ -15,12 +46,12 @@ db.once("open", async () => {
     const email = faker.internet.email(firstName);
     const password = faker.internet.password();
     const description = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
-    userData.push({ firstName, lastName, email, password, description });
+    const tags = generateTagString();
+    userData.push({ firstName, lastName, email, password, description, tags });
   }
 
   const createdUsers = await User.collection.insertMany(userData);
   console.log(createdUsers);
-  console.log("all done!");
+  console.log('all done!');
   process.exit(0);
 });
