0d4281eeebcdacc11a4dc48eb1c50c585770e392
shanemcf <shanemcfadden95@yahoo.com>
2021-08-22 16:10:29 -0500

    added functionality to connections on profile page

--- client/src/components/connections/index.js
+++ client/src/components/connections/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typography from "@material-ui/core/Typography";
 
-const Connections = ({ connectionCount, username, connections }) => {
+const Connections = ({ connectionCount, firstname, connections }) => {
   if (!connections || !connections.length) {
-    return <p className="bg-dark text-light p-3">{username} connect with developers!</p>;
+    return <Typography component="h1" variant="h5">{firstname} connect with developers!</Typography>;
   }
 
   return (
     <div>
       <h5>
-        {username}'s {connectionCount} {connectionCount === 1 ? 'connect' : 'connects'}
+        your {connectionCount} {connectionCount === 1 ? 'connect' : 'connections'}
       </h5>
       {connections.map(connection => (
         <button className="btn w-100 display-block mb-2" key={connection._id}>
-          <Link to={`/profile/${connection.username}`}>{connection.username}</Link>
+          <Link to={`/profile/${connection._id}`}>{connection._id}</Link>
         </button>
       ))}
     </div>
