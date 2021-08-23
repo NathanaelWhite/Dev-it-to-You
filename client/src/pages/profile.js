68463fc649552c7924873f06ae6568f49d588be8
shanemcf <shanemcfadden95@yahoo.com>
2021-08-22 21:39:01 -0500

    handling background and avatar issues overlapping text

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -17,23 +17,48 @@ import Container from "@material-ui/core/Container";
 import FingerprintIcon from "@material-ui/icons/Fingerprint";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
 // will be used if no profile picture
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
 const Profile = () => {
   const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      textAlign: "center",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#393E41",
+      color: "#F6F7EB",
+      "& div#user": {
+        backgroundColor: "#F6F7EB",
+      },
+    },
+
     paper: {
       marginTop: theme.spacing(14),
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      
     },
+    // avatar: {
+    //   margin: theme.spacing(1),
+    //   backgroundColor: theme.palette.primary.main,
+    //   alignItems: "center",
+    //   display: "flex",
+    //   flexWrap: "wrap",
+    // },
     avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.primary.main,
-      alignItems: "center",
-      display: 'flex',
-      flexWrap: 'wrap',
+      margin: theme.spacing(20),
+      width: "200px",
+      height: "150px",
+      borderRadius: "40%",
+      // position: "relative",
+      top: "230px",
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.secondary.main,
     },
     form: {
       width: "100%",
@@ -80,13 +105,9 @@ const Profile = () => {
   }
 
   // error handling login verification
-    if (!user?._id) {
-      return (
-        <Typography>
-          You need to be logged in to see this
-        </Typography>
-      );
-    }
+  if (!user?._id) {
+    return <Typography>You need to be logged in to see this</Typography>;
+  }
 
   const handleClick = async () => {
     try {
@@ -102,133 +123,100 @@ const Profile = () => {
     <Container component="main" maxWidth="xs" alignItems="center">
       <CssBaseline />
       {/* showing which user you are viewing */}
-      <div className={classes.paper}>
-      <Grid container spacing={6} alignItems="center">
-      <Box textAlign="center">
-        <Typography component="h1" variant="h5">
-          Viewing {id ? `${shownUser?.firstName}'s` : "your"} profile.
-        </Typography>
-        </Box >
-        <Avatar className={classes.avatar}>
-          <AccountBoxIcon fontSize="large" />
-        
-        {/* profile image */}
-        {/* <div id="profileimg" className={classes.paper}>
+      <div elevation={0} className={classes.root}>
+        <Grid container spacing={6} alignItems="center">
+          <div id="user">
+            <Avatar
+              alt="profile picture"
+              src="/broken-image.jpg"
+              className={classes.avatar}
+            >
+              <AccountBoxIcon fontSize="large" />
+            </Avatar>
+          </div>
+
+          <Divider />
+          {/* profile image */}
+          {/* <div id="profileimg" className={classes.paper}>
           <img src="" alt="profile-img" className={classes.imgStyles} />
         </div> */}
-        </Avatar>
-        {/* users first name */}
-        <Grid item xs={12}>
+
+          <Box textAlign="center">
+            <Typography component="h1" variant="h5">
+              Viewing {id ? `${shownUser?.firstName}'s` : "your"} profile.
+            </Typography>
+          </Box>
+
+          {/* users first name, not used because redundancy */}
+          {/* <Grid item xs={12}>
         <Typography component="h1" variant="h5">
          name: {shownUser?.firstName || "FirstName"}
         </Typography>
-        </Grid>
-        {/* description of user */}
-        <Grid item xs={12}>
-        <Typography>About: {shownUser?.description || "bio"}</Typography>
-        </Grid>
-        {/* users tags */}
-        <Grid item xs={12}>
-        <Typography>
-          
-          <FingerprintIcon />
-          Skills: {shownUser?.tags || "tags"}
-        </Typography>
-        </Grid>
-        {/* if connected, shows your connections email address on their page */}
-        {/* currently will show email address even if not connected */}
-        {data?.user &&  (
+        </Grid> */}
+          {/* description of user */}
+          <Box fontStyle="italic" lineHeight={5}>
+            About: {shownUser?.description || "bio"}
+          </Box>
+          {/* <Grid item xs={12}>
+            <Typography>About: {shownUser?.description || "bio"}</Typography>
+          </Grid> */}
+          {/* users tags */}
           <Grid item xs={12}>
             <Typography>
-              {shownUser?._id || "emailAddress"}
+              <FingerprintIcon />
+              Skills: {shownUser?.tags || "tags"}
             </Typography>
+          </Grid>
+          {/* if connected, shows your connections email address on their page */}
+          {/* currently will show email address even if not connected */}
+          <Grid item md={12} sm={12} xs={12}>
+            <Typography variant="caption">Email:</Typography>
+          </Grid>
+          {data?.user && (
+            <Grid item xs={12}>
+              <Typography>{shownUser?.email || "emailAddress"}</Typography>
             </Grid>
-        )} 
+          )}
 
-        {/* running case where if on own profile, can edit, 
+          {/* running case where if on own profile, can edit, 
       if on someone elses can add connection */}
-<<<<<<< HEAD
-        
-         {data?.user &&  (
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          onClick={handleClick}
-        >
-          Add Connection
-        </Button>
-        )} 
 
-        
-       
+          {data?.user && (
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleClick}
+            >
+              Add Connection
+            </Button>
+          )}
 
-        {/* functional, does not show count or names */}
-        <div className={classes.card}>
-          <Connections
-            username={shownUser.firstname}
-            connectionCount={shownUser.connectionCount}
-            connections={shownUser.connections}
-          />
-        </div>
+          {/* functional, does not show count or names */}
+          <div className={classes.card}>
+            <Connections
+              username={shownUser.firstname}
+              connectionCount={shownUser.connectionCount}
+              connections={shownUser.connections}
+            />
+          </div>
 
-          
-        {/* edit button for updating personal profile */}
-        {data?.me && (
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          onClick={handleClick}
-        >
-          edit profile
-        </Button>
-    )}
+          {/* edit button for updating personal profile */}
+          {data?.me && (
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleClick}
+            >
+              edit profile
+            </Button>
+          )}
         </Grid>
-=======
-
-        {data?.user && (
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleClick}
-          >
-            Add Connection
-          </Button>
-        )}
-
-        {/* edit button for updating personal profile */}
-        {data?.me && (
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleClick}
-            component={Link}
-            to={"/update"}
-          >
-            edit profile
-          </Button>
-        )}
-
-        {/* functional, not styled shows up as small bars */}
-        <div className={classes.paper}>
-          <Connections
-            username={shownUser.firstname}
-            connectionCount={shownUser.connectionCount}
-            connections={shownUser.connections}
-          />
-        </div>
->>>>>>> develop
       </div>
     </Container>
   );
