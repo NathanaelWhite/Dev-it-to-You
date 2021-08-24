aba1e4d48ff2faa73fae5c22225b73dca644d818
HP-Adam <adamhpeterman@gmail.com>
2021-08-23 19:38:31 -0500

    profile -> edit profile

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -212,6 +212,8 @@ const Profile = () => {
               color="primary"
               className={classes.submit}
               onClick={handleClick}
+              component={Link}
+              to={"/update"}
             >
               edit profile
             </Button>
